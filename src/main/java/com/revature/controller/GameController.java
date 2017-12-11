b0ca4bcc13e26bbe8d54d4ded249111b1fc0c014
GraveyGrumps <jakob4th@gmail.com>
2017-12-10 20:07:47 -0500

    solved conflicts

--- src/main/java/com/revature/controller/GameController.java
+++ src/main/java/com/revature/controller/GameController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.PutMapping;
+
 import com.revature.entities.Game;
 import com.revature.service.GameService;
 
@@ -73,7 +73,7 @@ public class GameController {
 	@ResponseBody
 	public Game modifyGame(@RequestBody Game game) {
 		log.info("Modifying a game");
-		return gs.modifyGame(game);
+		return gs.updateGame(game);
 	}
 
 	@PutMapping("update")
