0fbd676e4d561d5691b6470a13a30f00a44bab17
Chris Y <cmy1821@gmail.com>
2017-12-12 10:37:32 -0500

    game updated

--- BattleShip_Angular_4/src/app/games/battleship/testpannel/testpannel.component.ts
+++ BattleShip_Angular_4/src/app/games/battleship/testpannel/testpannel.component.ts
@@ -106,7 +106,6 @@ export class TestPannelComponent implements OnInit {
     if (this.currGame.turn) {
       this.currGame.status = 'inprogress';
     }
-    
     this.turnSwap();
     this.update();
   }
